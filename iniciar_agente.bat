b0a690972882772e671824e7aba05281ad1c1b53
AngelRamosv <angeluam.0927@gmail.com>
2026-01-20 01:09:57 -0600

    actualizacion agenteprox

--- iniciar_agente.bat
+++ iniciar_agente.bat
@@ -1,6 +1,3 @@
 @echo off
-:: Cambiar al directorio donde esta este archivo
 cd /d "%~dp0"
-
-:: Ejecutar el agente (Python debe estar en el PATH o usa la ruta completa si falla)
 python vm_agent.py
